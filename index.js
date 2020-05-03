19486aad7052bcc47bfa404dfcfe856e7627ea2d
flatironschool <t.benson@live.co.uk>
2020-05-03 14:45:09 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+const map = (arr, callback) => {
+    const mapped = []
+    for (const num of arr) {
+        mapped.push(callback(num))
+    }
+    return mapped
+}
+
+const reduce = (arr, callback, start) => {
+    let reduced = !!start ? start : arr[0]
+
+    let i = !!start ? 0 : 1
+    for (; i < arr.length; i++) {
+        reduced = callback(arr[i], reduced)
+    }
+    return reduced
+}
